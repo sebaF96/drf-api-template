da88b3379c6da7463608ec0d09d165b601ea356d
sebaf96 <sefernandez96@gmail.com>
2021-02-26 00:22:13 -0300

    Add CORS whitelist for React app

--- api/settings.py
+++ api/settings.py
@@ -132,6 +132,11 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': datetime.timedelta(hours=1)
 }
 
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
